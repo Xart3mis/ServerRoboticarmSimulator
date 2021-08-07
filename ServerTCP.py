aa9bb3da390d19017a9659ef13a7b5d996cdeb82
Xart3mis <xart3misx@protonmail.com>
2021-08-07 19:52:40 +0200

    robodk

--- ServerTCP.py
+++ ServerTCP.py
@@ -7,37 +7,37 @@ currentPath = os.path.dirname(os.path.realpath(__file__))
 
 os.chdir(currentPath)
 
-# RDK = Robolink(
-#     args=[
-#         "-NOSPLASH",
-#         "-TREE_STATE=0",
-#         "-EXIT_LAST_COMM",
-#         "-NOUI",
-#         "RoboDk/station.rdk",
-#     ],
-# )
+RDK = Robolink(
+    args=[
+        "-NOSPLASH",
+        "-TREE_STATE=0",
+        "-EXIT_LAST_COMM",
+        "-NOUI",
+        "RoboDk/station.rdk",
+    ],
+)
 
-# RDK.Render(False)
-# RDK.setRunMode(RUNMODE_SIMULATE)
-# RDK.setCollisionActive(COLLISION_OFF)
-# RDK.setWindowState(WINDOWSTATE_MAXIMIZED)
+RDK.Render(False)
+RDK.setRunMode(RUNMODE_SIMULATE)
+RDK.setCollisionActive(COLLISION_OFF)
+RDK.setWindowState(WINDOWSTATE_MAXIMIZED)
 
-# prog = RDK.AddProgram("AutoProgram")
-# prog.ShowInstructions(False)
+prog = RDK.AddProgram("AutoProgram")
+prog.ShowInstructions(False)
 
-# robot = RDK.Item("K1.1 Osiris", ITEM_TYPE_ROBOT)
-# # robot.setParam("PostProcessor", "K1 Osiris Post.py")
+robot = RDK.Item("K1.1 Osiris", ITEM_TYPE_ROBOT)
+# robot.setParam("PostProcessor", "K1 Osiris Post.py")
 
-# robot.setPoseFrame(robot.PoseFrame())
-# robot.setPoseTool(robot.PoseTool())
-# moveSpeed = 50
-
-
-# def Home(rbt):
-#     rbt.MoveJ(robot.JointsHome())
+robot.setPoseFrame(robot.PoseFrame())
+robot.setPoseTool(robot.PoseTool())
+moveSpeed = 50
 
 
-# Home(robot)
+def Home(rbt):
+    rbt.MoveJ(robot.JointsHome())
+
+
+Home(robot)
 
 port = 2992
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,8 +52,11 @@ with conn:
     while True:
         data = str(conn.recv(1024), "utf-8")
         print(data)
-        # if not prog.Busy() and not robot.Busy():
-        if True:
+
+        if len(data) > 2:
+            data = data[0:2]
+
+        if not prog.Busy() and not robot.Busy():
             move_direction = [0, 0, 0]
 
             if data == "Y-":
@@ -68,8 +71,8 @@ with conn:
                 move_direction = [0, 0, 1]
             elif data == "Z-":
                 move_direction = [0, 0, -1]
-            # elif data == "home":
-            #     Home(robot)
+            elif data == "home":
+                Home(robot)
 
             if norm(move_direction) <= 0:
                 continue
@@ -77,23 +80,23 @@ with conn:
             print(data)
             print(move_direction)
 
-            # xyz_move = mult3(move_direction, moveSpeed)
+            xyz_move = mult3(move_direction, moveSpeed)
 
-            # robot_joints = robot.Joints()
+            robot_joints = robot.Joints()
 
-            # robot_position = robot.SolveFK(robot_joints)
+            robot_position = robot.SolveFK(robot_joints)
 
-            # robot_config = robot.JointsConfig(robot_joints)
+            robot_config = robot.JointsConfig(robot_joints)
 
-            # new_robot_position = transl(xyz_move) * robot_position
+            new_robot_position = transl(xyz_move) * robot_position
 
-            # new_robot_joints = robot.SolveIK(new_robot_position)
-            # if len(new_robot_joints.tolist()) < 6:
-            #     print(
-            #         "No robot solution!! The new position is too far, out of reach or close to a singularity"
-            #     )
-            #     continue
+            new_robot_joints = robot.SolveIK(new_robot_position)
+            if len(new_robot_joints.tolist()) < 6:
+                print(
+                    "No robot solution!! The new position is too far, out of reach or close to a singularity"
+                )
+                continue
 
-            # new_robot_config = robot.JointsConfig(new_robot_joints)
+            new_robot_config = robot.JointsConfig(new_robot_joints)
 
-            # robot.MoveJ(new_robot_joints)
+            robot.MoveJ(new_robot_joints)
