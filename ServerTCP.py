8dec570d9bfc85c0e3744bcb6b301db14a3eb37e
Xart3mis <Xart3misX@protonmail.com>
2021-08-09 15:24:54 +0200

    sex

--- ServerTCP.py
+++ ServerTCP.py
@@ -113,6 +113,6 @@ with conn:
 
             try:
                 # bus.write_i2c_block_data(i2cAddr, 0, [round(i) for i in jointAngles])
-                bus.write_i2c_block_data(i2cAddr, 0, round(jointAngles[0]))
+                bus.write_i2c_block_data(i2cAddr, 0, [round(jointAngles[0])])
             except OSError:
                 print("could not communicate with i2c bus")
